72b697255a9735acabdc93b2af1a651b87211964
Heartsonsunday <leahgdg@hotmail.com>
2019-05-03 23:45:07 -0400

    building express app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+var fs = require('fs');
+
+
+fs.readfile("Database.txt", "utf8", function(err, data) {
+    if (err) throw err;
+    console.log(data);
+} )
+
+
+
+
+
+
+console.log("lets party");
+
+const port = process.env.PORT || 5502;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
